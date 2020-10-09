34b2ade62d109be0ac5f5e65c0451f86f73d2c6d
looping-torrent-8105 <gianherrera96@gmail.com>
2020-10-09 03:14:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ function takeANumber(katzDeliLine, name)
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
+
+var nowServing (katzDeliLine) => {
+  if (katzDeliLine.length > 0) {
+     var mostrar = katzDeliLine[0];
+     mostrar.shift();
+     return `Atendiendo ${mostrar}.`;
+  }else {
+     return `¡No hay nadie esperando a ser atendido!!!`;
+  }
+}
